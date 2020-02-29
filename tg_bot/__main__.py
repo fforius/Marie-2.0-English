8d89c3e81e8425848362d5c1a876b231b9f6384c
fforius <60130637+fforius@users.noreply.github.com>
2020-02-29 09:57:02 +0700

    Revert "Update __main__.py"
    This reverts commit ca177f85f6c05a03a5f51f68c610e94770d4cdd2.

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,12 +18,13 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
 
-Im a group manager bot maintained by [this person](tg://user?id={}) yang sangat tampan.
+im a group manager bot maintained by  [this person](tg://user?id={}) yang sangat tampan.
 
-For Support Group tap [Here](t.me/officialclubz)
 For more commands click /help...
+
+**.**
 
 """
 
@@ -44,7 +45,7 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/apaloeliat) to get me to where I am now, and every donation helps \
+It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
 There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
